139faf9f132b083ad6eaa2d509b9dc7bcb71b724
Michael Humphrey <michaelhumph@gmail.com>
2025-09-02 21:58:39 +0000

    The NextJS app server is having trouble starting. Please identify what c

---
+++ src/next.config.ts
@@ -0,0 +1,41 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  // This is required to allow the Next.js dev server to accept requests from the
+  // Cloud Workstations proxy.
+  allowedDevOrigins: [
+    'https://*.cloudworkstations.dev',
+  ],
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'encrypted-tbn0.gstatic.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
